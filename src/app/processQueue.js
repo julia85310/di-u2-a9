093f364998c77299b84c3feb54407625b9c86131
julia85310 <juliarueda85310@gmail.com>
2024-11-18 11:17:53 +0100

    terminado

--- src/app/processQueue.js
+++ src/app/processQueue.js
@@ -2,7 +2,13 @@ export function getFinalState(baseState, queue) {
     let finalState = baseState;
   
     // TODO: haz algo con la cola...
-  
+    for(let i = 0; i < queue.length; i++){
+      if(typeof queue[i] == 'function'){
+        finalState = queue[i](finalState);
+      }else{
+        finalState = queue[i];
+      }
+    }
     return finalState;
   }
   
